aad22a2afd6138c0fbc706411a8f089024308644
Albertalandia <75736179+Albertalandia@users.noreply.github.com>
2021-02-17 22:50:56 -0300

    Clase 11. jQuery

--- script.js
+++ script.js
@@ -4,26 +4,39 @@
             nombre: "Lucas",
             dinero: 50000, 
             meses: 12, 
-            intereses: parseInt((dinero * 5)/100),
+            intereses: parseInt((50000 * 5)/100),
         }, {
             nombre: "Daniela",
             dinero: 100000, 
             meses: 10, 
-            intereses: parseInt(dinero * 5)/100,
+            intereses: parseInt(10000 * 5)/100,
         },
             {
             nombre: "Lisandro",
             dinero: 60000, 
             meses: 12, 
-            intereses: parseInt((dinero * 5)/100),
+            intereses: parseInt((60000 * 5)/100),
         }
     ]; 
+
+    function prestamo (nombre, dinero, meses, intereses) {
+
+        this.nombre = nombre;
+        this.dinero = dinero;
+        this.meses = meses;
+        this.intereses = intereses;
+
+        this.informacion = function(){
+            return infoCuotaPrestamo(this)
+        };
+    }
     
     function recogerDatos() {
 
     var nombre = document.getElementById("nombre").value;
     var dinero =  parseInt(document.getElementById("dinero").value);
     var meses = parseInt(document.getElementById("meses").value);
+
 
     var cliente = "";
     var clienteSi = document.getElementById("si");
@@ -41,20 +54,12 @@
         return prestamo.dinero + " " + prestamo.intereses
     }
 
-    function prestamo (nombre, dinero, meses, intereses) {
 
-        this.nombre = nombre;
-        this.dinero = dinero;
-        this.meses = meses;
-        this.intereses = intereses;
-
-        this.informacion = function(){
-            return infoCuotaPrestamo(this)
-        };
-    }
 
     var miPrestamo = new prestamo (nombre, dinero, meses, intereses);
     todosLosPrestamos.push(new prestamo(nombre, dinero, meses, intereses));
+    sessionStorage.setItem("JSON", JSON.stringify( todosLosPrestamos))
+
     console.log(todosLosPrestamos)
 
     var descuento = 1000;
@@ -63,15 +68,25 @@
     function sacarCuotaFinal(){
         
             if ( cliente ==="si") {
-                miCuotaConDescuento;
-        }   else if (cliente === "no"){
-            cuotaPrestamo;
-        }   return miCuotaConDescuento;
+                return miCuotaConDescuento;
+        }   else {
+                return cuotaPrestamo;
+        }  
     }
 
 
     var parrafo = document.createElement("p");
-    var contenido = document.createTextNode("Tu cuota será de " + sacarCuotaFinal()); 
+    var contenido = document.createTextNode("Me tenes que dar esto por mes: " + sacarCuotaFinal()); 
     parrafo.appendChild(contenido);
     document.body.appendChild(parrafo);
 }
+
+window.addEventListener('load', function () {
+    console.log('Hola!');
+  })
+  
+
+$( document ).ready(function() {
+   console.log( "Ya cargo todo" );
+});
+
